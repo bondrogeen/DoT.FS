663ed12a0791f1c0a3c7874338665f44aaecc009
bondrogeen <bondrogeen@gmail.com>
2018-04-12 12:53:55 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# DoT
+ 
+The plugin FS for [DoT](https://github.com/bondrogeen/DoT) File editor. (developing) 
+
+## Changelog
+
+### 0.0.1 (2018-04-12)
+* (bondrogeen) init.
+
+
+
---
+++ files/FS.lua
@@ -0,0 +1,22 @@
+local function list()
+
+ end
+
+local function del()
+
+ end
+
+local function add()
+
+ end
+
+local function list()
+
+end
+
+return function (t)
+ local r
+
+
+ return r
+end
